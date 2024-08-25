e4fe5e13958a51af1e532205bfbfae3190bdac3e
Permafrozen <m8tt8o@gmail.com>
2024-08-25 12:31:13 +0200

    deleted some comments

--- modules/common/home-manager.nix
+++ modules/common/home-manager.nix
@@ -11,14 +11,6 @@ in
   # Home Manager release for this config, do not change without reading release notes!!!
   home.stateVersion = "24.05";
 
-  # Packages, these will be combined with the ones in configuration.nix and the home.packages of other files
-  # home.packages = [
-  #  pkgs.kitty
-  #  pkgs.fish
-  #  pkgs.qutebrowser
-  #  pkgs.acpi
-  # ];
-
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
