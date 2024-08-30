ca91919f21bb5f0edca9c4080cc685a08b69f9d2
Permafrozen <m8tt8o@gmail.com>
2024-08-30 19:49:19 +0200

    Changed name of root folder to .modular-nixos-configuration for consistency

--- modules/user/utilities/hyprpaper.nix
+++ modules/user/utilities/hyprpaper.nix
@@ -9,13 +9,13 @@
       splash_offset = 2.0;
 
       preload =
-      ["~/.nixos-modular-configuration/modules/dotfiles/wallpapers/nixos-background-dark.png"
-       "~/.nixos-modular-configuration/modules/dotfiles/wallpapers/purple-leaves.png"
+      ["~/.modular-nixos-configuration/modules/dotfiles/wallpapers/nixos-background-dark.png"
+       "~/.modular-nixos-configuration/modules/dotfiles/wallpapers/purple-leaves.png"
       ];
 
       wallpaper = [
-        # "eDP-1,~/.nixos-modular-configuration/modules/dotfiles/wallpapers/nixos-background-dark.png"
-        "eDP-1,~/.nixos-modular-configuration/modules/dotfiles/wallpapers/purple-leaves.png"
+        # "eDP-1,~/.modular-nixos-configuration/modules/dotfiles/wallpapers/nixos-background-dark.png"
+        "eDP-1,~/.modular-nixos-configuration/modules/dotfiles/wallpapers/purple-leaves.png"
       ];
     };
   };
