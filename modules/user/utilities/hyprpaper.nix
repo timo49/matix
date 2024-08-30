66b66445016e4f2779e102f0ab0e17c12ecf38ba
Permafrozen <m8tt8o@gmail.com>
2024-08-30 19:40:29 +0200

    Added and selected new wallpaper "purple-leaves.png"

--- modules/user/utilities/hyprpaper.nix
+++ modules/user/utilities/hyprpaper.nix
@@ -9,10 +9,13 @@
       splash_offset = 2.0;
 
       preload =
-      ["~/.nixos-modular-configuration/modules/dotfiles/wallpapers/nixos-background-dark.png"];
+      ["~/.nixos-modular-configuration/modules/dotfiles/wallpapers/nixos-background-dark.png"
+       "~/.nixos-modular-configuration/modules/dotfiles/wallpapers/purple-leaves.png"
+      ];
 
       wallpaper = [
-        "eDP-1,~/.nixos-modular-configuration/modules/dotfiles/wallpapers/nixos-background-dark.png"
+        # "eDP-1,~/.nixos-modular-configuration/modules/dotfiles/wallpapers/nixos-background-dark.png"
+        "eDP-1,~/.nixos-modular-configuration/modules/dotfiles/wallpapers/purple-leaves.png"
       ];
     };
   };
