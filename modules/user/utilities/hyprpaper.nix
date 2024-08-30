a71326e1351924cdcc43b3f52ed2e3b29c43b4f9
Permafrozen <m8tt8o@gmail.com>
2024-08-30 20:20:43 +0200

    Added and changed to another Wallpaper in hyprpaper.nix

--- modules/user/utilities/hyprpaper.nix
+++ modules/user/utilities/hyprpaper.nix
@@ -8,14 +8,16 @@
       splash = false;
       splash_offset = 2.0;
 
-      preload =
-      ["~/.modular-nixos-configuration/modules/dotfiles/wallpapers/nixos-background-dark.png"
-       "~/.modular-nixos-configuration/modules/dotfiles/wallpapers/purple-leaves.png"
+      preload = [
+        "~/.modular-nixos-configuration/modules/dotfiles/wallpapers/nixos-background-dark.png"
+        "~/.modular-nixos-configuration/modules/dotfiles/wallpapers/purple-leaves.png"
+        "~/.modular-nixos-configuration/modules/dotfiles/wallpapers/dark-fantasy.png"
       ];
 
       wallpaper = [
         # "eDP-1,~/.modular-nixos-configuration/modules/dotfiles/wallpapers/nixos-background-dark.png"
-        "eDP-1,~/.modular-nixos-configuration/modules/dotfiles/wallpapers/purple-leaves.png"
+        # "eDP-1,~/.modular-nixos-configuration/modules/dotfiles/wallpapers/purple-leaves.png"
+        "eDP-1,~/.modular-nixos-configuration/modules/dotfiles/wallpapers/dark-fantasy.png"
       ];
     };
   };
