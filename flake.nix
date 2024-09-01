82f7d1b08efddfe25e6d59c31abfaa43ce5a1d93
Permafrozen <m8tt8o@gmail.com>
2024-09-01 13:14:46 +0200

    added nix-colors as a flake input & output

--- flake.nix
+++ flake.nix
@@ -4,10 +4,13 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-24.05";
     home-manager.url = "github:nix-community/home-manager/release-24.05";
+    nix-colors.url = "github:misterio77/nix-colors";
+
+
     home-manager.inputs.nixpkgs.follows = "nixpkgs"; # avoids version conflicts
   };
 
-  outputs = { self, nixpkgs, home-manager, ...}:
+  outputs = { self, nixpkgs, home-manager, nix-colors, ...}:
     let
       lib = nixpkgs.lib;
       system = "x86_64-linux";
@@ -27,6 +30,8 @@
       ${vars.username} = home-manager.lib.homeManagerConfiguration {
         inherit pkgs; # passes declaration of the "pkgs" in the let binding as an argument
         modules = [ ./home.nix ];
+
+        extraSpecialArgs = { inherit nix-colors; }; # color-scheme managing
       };
     };
   };
