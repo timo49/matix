2271379e795e9f14c546b061ae8ab1ec37f08e7b
Permafrozen <m8tt8o@gmail.com>
2024-09-03 15:53:38 +0200

    added pipewire and wireplumber

--- modules/system/services.nix
+++ modules/system/services.nix
@@ -2,5 +2,19 @@
 
 {
 # Enable the OpenSSH daemon.
-   services.openssh.enable = true;
+  services.openssh.enable = true;
+
+  services.pipewire = {
+    enable = true;
+    wireplumber = {
+      enable = true;
+      extraConfig = {
+        "10-disable-camera" = {
+          "wireplumber.profiles" = {
+            main."monitor.libcamera" = "disabled";
+	  };
+	};
+      };
+    };
+  };
 }
