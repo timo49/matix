457e5f1cc2e7bbc3ffdd50a12c05131ed0d153c1
Permafrozen <m8tt8o@gmail.com>
2024-09-05 20:08:08 +0200

    added neovim configuration

--- modules/user/editor/neovim.nix
+++ modules/user/editor/neovim.nix
@@ -3,5 +3,29 @@
 {
   programs.neovim = {
     enable = true;
+
+    extraLuaConfig = ''
+      -- Hint: use `:h <option>` to figure out the meaning if needed
+      vim.opt.clipboard = 'unnamedplus'   -- use system clipboard 
+      vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+      vim.opt.mouse = 'a'                 -- allow the mouse to be used in Nvim
+
+      -- Tab
+      vim.opt.tabstop = 2                 -- number of visual spaces per TAB
+      vim.opt.softtabstop = 2             -- number of spacesin tab when editing
+      vim.opt.shiftwidth = 2              -- insert 4 spaces on a tab
+      vim.opt.expandtab = true            -- tabs are spaces, mainly because of python
+
+      -- UI config
+      vim.opt.number = true               -- show absolute number
+      vim.opt.termguicolors = true
+
+      -- Searching
+      vim.opt.incsearch = true            -- search as characters are entered
+      vim.opt.hlsearch = false            -- do not highlight matches
+      vim.opt.ignorecase = true           -- ignore case in searches by default
+      vim.opt.smartcase = true            -- but make it case sensitive if an uppercase is entered
+
+    '';
   };
 }
