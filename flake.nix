296dd588bb97fabbbeed552e1cdc60c349e017e3
Permafrozen <m8tt8o@gmail.com>
2024-09-05 19:24:31 +0200

    removed swp file, small changes to flake.nix

--- flake.nix
+++ flake.nix
@@ -3,13 +3,12 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-24.05";
+    inputs.nixpkgs.follows = "nixpkgs";
 
     home-manager = { 
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    nix-colors.url = "github:misterio77/nix-colors";
   };
 
   outputs = { self, nixpkgs, home-manager, nix-colors, ...}:
@@ -18,22 +17,20 @@
       lib = nixpkgs.lib;
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system}; #package architecture for homemanager
+
       extraSpecialArgs = { inherit system; inherit pkgs;  inherit nix-colors; };  # <- passing inputs to the attribute set for home-manager
       specialArgs = { inherit system; }; # <- same but for Nixos
     in {
     nixosConfigurations = {
       nixos = lib.nixosSystem {
         inherit specialArgs;
-
         modules = [ ./configuration.nix ];
-
       };
     };
    
     homeConfigurations = {
       ${vars.username} = home-manager.lib.homeManagerConfiguration {
         inherit extraSpecialArgs;
-
 	modules = [ ./home.nix ];
       };
     };
