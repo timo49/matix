9690058dd07d605a16406e42b17d7baeb59c88dd
Permafrozen <m8tt8o@gmail.com>
2024-09-06 10:45:29 +0200

    tested some hexToRgb function

--- modules/common/variables.nix
+++ modules/common/variables.nix
@@ -22,4 +22,18 @@
   gitMail = "m8tt8o@gmail.com";
   defaultBranch = "main";
 
+  # Hex -> Rgb
+  hexToRgb = hex: let
+    # Remove the hash prefix if present
+    hexColor = if builtins.substring 0 1 hex == "#" then builtins.substring 1 (builtins.stringLength hex - 1) hex else hex;
+    r = builtins.parseInt (builtins.substring 0 2 hexColor) 16;
+    g = builtins.parseInt (builtins.substring 2 2 hexColor) 16;
+    b = builtins.parseInt (builtins.substring 4 2 hexColor) 16;
+  in
+    {
+      red = r;
+      green = g;
+      blue = b;
+    };
+
 }
