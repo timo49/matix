64d2418c98d9195c367c8c298d9d1958cac218a5
Permafrozen <m8tt8o@gmail.com>
2024-09-07 13:59:59 +0200

    home-manager stylix core config added

--- modules/system/style/stylix.nix
+++ modules/system/style/stylix.nix
@@ -7,17 +7,20 @@ in
   stylix = {
     enable = true;
     autoEnable = false;
-    
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/${vars.colorScheme}.yaml";
     image = /home/${vars.username}/${vars.wallpaperPath};
+
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/${vars.colorScheme}.yaml";
 
   };
 
   home-manager.sharedModules = [{
     stylix = {
       enable = true;
-
       autoEnable = false;
+      stylix.image = /home/${vars.username}/${vars.wallpaperPath};
+
+      base16Scheme = "${pkgs.base16-schemes}/share/themes/${vars.colorScheme}.yaml";
+
     };
   }];
 }
