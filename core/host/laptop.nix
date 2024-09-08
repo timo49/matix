bc0d06e66534a87ad7b1637c0f41479dda0bde01
Permafrozen <m8tt8o@gmail.com>
2024-09-08 17:33:18 +0200

    added dunst for real this time

--- core/host/laptop.nix
+++ core/host/laptop.nix
@@ -9,6 +9,7 @@
     ../../external/config/desktop-environment/window-manager/hyprland.nix
     ../../external/config/desktop-environment/status-bar/waybar.nix
     ../../external/config/desktop-environment/wallpaper-setter/hyprpaper.nix
+    ../..//external/config/desktop-environment/notification-daemon/dunst.nix
 
   ];
 }
