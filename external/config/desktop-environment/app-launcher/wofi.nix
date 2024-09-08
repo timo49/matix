66b4121d9dd680b66fbba80accc076047c70c4b6
Permafrozen <m8tt8o@gmail.com>
2024-09-08 17:37:25 +0200

    added wofi

--- external/config/desktop-environment/app-launcher/wofi.nix
+++ external/config/desktop-environment/app-launcher/wofi.nix
@@ -1,43 +1,50 @@
 { config, pkgs, ... }:
 
 let
-  vars = import ../../common/variables.nix;
+  vars = import ../../../../core/system/variables.nix;
 in {
 
-  programs.wofi = {
-    enable = true;
+  environment.systemPackages = with pkgs; [
+    wofi
+  ];
 
-    settings = {
-      hide_scoll = true;
-      allow_markup = true;
+  home-manager.sharedModules = [{
+
+    programs.wofi = {
+      enable = true;
+
+      settings = {
+        hide_scoll = true;
+        allow_markup = true;
+      };
+
+      style  = ''
+        * {
+          font-family: Hack Nerd Font;
+	        color: #${config.lib.stylix.colors.base05};
+	        border: none;
+        }
+
+        #window {
+          background: rgba(${config.lib.stylix.colors.base00-rgb-r}, ${config.lib.stylix.colors.base00-rgb-g}, ${config.lib.stylix.colors.base00-rgb-b}, ${vars.opacity});
+	        border: 2px solid #${config.lib.stylix.colors.base03};
+        }
+
+        #outer-box {
+	        border: 2px solid #${config.lib.stylix.colors.base03};
+        }
+
+        #input {
+          background: #${config.lib.stylix.colors.base00};
+	        border: none;
+        }
+
+        #entry:selected {
+          background: #${config.lib.stylix.colors.base03};
+	        color: #${config.lib.stylix.colors.base04}:
+	        border: none;
+        }
+      '';
     };
-
-    style  = ''
-      * {
-        font-family: Hack Nerd Font;
-	      color: #${config.lib.stylix.colors.base05};
-	      border: none;
-      }
-
-      #window {
-        background: rgba(${config.lib.stylix.colors.base00-rgb-r}, ${config.lib.stylix.colors.base00-rgb-g}, ${config.lib.stylix.colors.base00-rgb-b}, ${vars.opacity});
-	      border: 2px solid #${config.lib.stylix.colors.base03};
-      }
-
-      #outer-box {
-	      border: 2px solid #${config.lib.stylix.colors.base03};
-      }
-
-      #input {
-        background: #${config.lib.stylix.colors.base00};
-	      border: none;
-      }
-
-      #entry:selected {
-        background: #${config.lib.stylix.colors.base03};
-	      color: #${config.lib.stylix.colors.base04}:
-	      border: none;
-      }
-    '';
-  };
+  }];
 }
