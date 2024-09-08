65e0f8495783f374b44ca6dd2571196fc81d86c3
Permafrozen <m8tt8o@gmail.com>
2024-09-08 17:32:26 +0200

    added dunst to host

--- external/config/desktop-environment/notification-daemon/dunst.nix
+++ external/config/desktop-environment/notification-daemon/dunst.nix
@@ -1,27 +1,34 @@
 { config, pkgs, lib, ...}:
 
 let
-  vars = import ../../common/variables.nix;
+  vars = import ../../../../core/system/variables.nix;
 in {
+    
+    environment.systemPackages = with pkgs; [
+      dunst
+    ];
+
+    home-manager.sharedModules = [{
 
     services.dunst = {
-    enable = true;
-    settings = {
-      global = {
-        width = 300;
-        height = 300;
-        offset = "20x20";
-        origin = "top-right";
-        transparency = "${vars.opacity}";
-        frame_color = lib.mkDefault "#${config.lib.stylix.colors.base03}";
-        font = lib.mkDefault "Hack Nerd Font 9";
-      };
+      enable = true;
+      settings = {
+        global = {
+          width = 300;
+          height = 300;
+          offset = "20x20";
+          origin = "top-right";
+          transparency = "${vars.opacity}";
+          frame_color = lib.mkDefault "#${config.lib.stylix.colors.base03}";
+          font = lib.mkDefault "Hack Nerd Font 9";
+        };
 
-      urgency_normal = {
-        background = lib.mkDefault "#${config.lib.stylix.colors.base00}";
-        foreground = lib.mkDefault "#${config.lib.stylix.colors.base05}";
-        timeout = 10;
+        urgency_normal = {
+          background = lib.mkDefault "#${config.lib.stylix.colors.base00}";
+          foreground = lib.mkDefault "#${config.lib.stylix.colors.base05}";
+          timeout = 10;
+        };
       };
     };
-  };
+  }];
 }
