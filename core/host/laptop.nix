6deb0139c5958b54248a46df386653e5ec8c005f
Permafrozen <m8tt8o@gmail.com>
2024-09-08 17:28:54 +0200

    fixxed spelling error

--- core/host/laptop.nix
+++ core/host/laptop.nix
@@ -6,9 +6,9 @@
     ../../external/config/color-manager/stylix.nix
 
     # Desktop Enviroment
-    ../../external/config/desktop-enviroment/window-manager/hyprland.nix
-    ../../external/config/desktop-enviroment/status-bar/waybar.nix
-    ../../external/config/desktop-enviroment/wallpaper-setter/hyprpaper.nix
+    ../../external/config/desktop-environment/window-manager/hyprland.nix
+    ../../external/config/desktop-environment/status-bar/waybar.nix
+    ../../external/config/desktop-environment/wallpaper-setter/hyprpaper.nix
 
   ];
 }
