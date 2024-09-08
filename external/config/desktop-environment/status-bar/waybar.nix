503855b6871c60b0ed079cb53a588f586a69cc8f
Permafrozen <m8tt8o@gmail.com>
2024-09-08 18:29:48 +0200

    fixxed waybar bug

--- external/config/desktop-environment/status-bar/waybar.nix
+++ external/config/desktop-environment/status-bar/waybar.nix
@@ -4,96 +4,96 @@ let
   vars = import ../../../../core/system/variables.nix;
 in {
 
-  programs.waybar = {
-    enable = true;
-  };
-
   environment.systemPackages = with pkgs; [
     waybar
   ];
 
   home-manager.sharedModules = [{
-  
-  # Cascading Style Sheet
-  home.file.".config/waybar/style.css".text = ''
-  * {
-    border: none;
-    border-radius: 0;
-    font-family: Hack Nerd Font;
-  }
 
-  #battery,
-  #clock,
-  #wireplumber {
-    padding: 0 10px;
-    margin: 0 5px;
-  }
-
-  window#waybar {
-    background: rgba(${config.lib.stylix.colors.base00-rgb-r}, ${config.lib.stylix.colors.base00-rgb-g}, ${config.lib.stylix.colors.base00-rgb-b}, ${vars.opacity});
-    color: #${config.lib.stylix.colors.base05};
-    border-bottom: 2px solid #${config.lib.stylix.colors.base03};
-  }
-
-  tooltip {
-    background: #${config.lib.stylix.colors.base00};
-    border: 2px solid #${config.lib.stylix.colors.base03};
-  }
-
-  #workspaces button {
-    color: #${config.lib.stylix.colors.base05};
-  }
-
-  #workspaces button.active {
-    color: #${config.lib.stylix.colors.base04};
-    border-bottom: 2px solid #${config.lib.stylix.colors.base04};
-  }
-  '';
-
-  # Configuration
-  home.file.".config/waybar/config.jsonc".text = ''
-    {
-      "layer": "top",
-      "position": "top",
-
-      "modules-left": [
-	"hyprland/workspaces",
-      ],
-
-      "modules-center": [
-	  "hyprland/window"
-      ],
-
-      "modules-right": [
-        "wireplumber",
-	"battery",
-        "clock"
-      ],
-
-      "hyprland/window": {
-          "format": "{}",
-          "max-length": 50
-      },
-
-      "battery": {
-          "format": "{icon} {capacity}%",
-          "format-icons": ["󰂃", "󰂃", "󰁼", "󰁽", "󰁾", "󰁿", "󰂀", "󰂁", "󰂂", "󰁹"],
-          "tooltip-format": "{time}"
-      },
-
-      "clock": {
-          "format": "󱑆 {:%H:%M }",
-          "format-alt": "󱑆 {:%a, %d. %b  %H:%M }",
-     },
-
-      "wireplumber": {
-        "format": "{icon} {volume}% ",
-        "format-muted": " ",
-        "scroll-step": "1",
-        "format-icons": ["", " ", " "]
-      }
+    programs.waybar = {
+      enable = true;
+    };
+    
+    # Cascading Style Sheet
+    home.file.".config/waybar/style.css".text = ''
+    * {
+      border: none;
+      border-radius: 0;
+      font-family: Hack Nerd Font;
     }
-  '';
+
+    #battery,
+    #clock,
+    #wireplumber {
+      padding: 0 10px;
+      margin: 0 5px;
+    }
+
+    window#waybar {
+      background: rgba(${config.lib.stylix.colors.base00-rgb-r}, ${config.lib.stylix.colors.base00-rgb-g}, ${config.lib.stylix.colors.base00-rgb-b}, ${vars.opacity});
+      color: #${config.lib.stylix.colors.base05};
+      border-bottom: 2px solid #${config.lib.stylix.colors.base03};
+    }
+
+    tooltip {
+      background: #${config.lib.stylix.colors.base00};
+      border: 2px solid #${config.lib.stylix.colors.base03};
+    }
+
+    #workspaces button {
+      color: #${config.lib.stylix.colors.base05};
+    }
+
+    #workspaces button.active {
+      color: #${config.lib.stylix.colors.base04};
+      border-bottom: 2px solid #${config.lib.stylix.colors.base04};
+    }
+    '';
+
+    # Configuration
+    home.file.".config/waybar/config.jsonc".text = ''
+      {
+        "layer": "top",
+        "position": "top",
+
+        "modules-left": [
+    "hyprland/workspaces",
+        ],
+
+        "modules-center": [
+      "hyprland/window"
+        ],
+
+        "modules-right": [
+          "wireplumber",
+    "battery",
+          "clock"
+        ],
+
+        "hyprland/window": {
+            "format": "{}",
+            "max-length": 50
+        },
+
+        "battery": {
+            "format": "{icon} {capacity}%",
+            "format-icons": ["󰂃", "󰂃", "󰁼", "󰁽", "󰁾", "󰁿", "󰂀", "󰂁", "󰂂", "󰁹"],
+            "tooltip-format": "{time}"
+        },
+
+        "clock": {
+            "format": "󱑆 {:%H:%M }",
+            "format-alt": "󱑆 {:%a, %d. %b  %H:%M }",
+       },
+
+        "wireplumber": {
+          "format": "{icon} {volume}% ",
+          "format-muted": " ",
+          "scroll-step": "1",
+          "format-icons": ["", " ", " "]
+        }
+      }
+    '';
 
   }];
 }
