ea5160a05a69ca1f1caa93e7bdf11ddde43db43b
Permafrozen <m8tt8o@gmail.com>
2024-09-08 17:33:55 +0200

    dunst working now

--- core/host/laptop.nix
+++ core/host/laptop.nix
@@ -9,7 +9,7 @@
     ../../external/config/desktop-environment/window-manager/hyprland.nix
     ../../external/config/desktop-environment/status-bar/waybar.nix
     ../../external/config/desktop-environment/wallpaper-setter/hyprpaper.nix
-    ../..//external/config/desktop-environment/notification-daemon/dunst.nix
+    ../../external/config/desktop-environment/notification-daemon/dunst.nix
 
   ];
 }
