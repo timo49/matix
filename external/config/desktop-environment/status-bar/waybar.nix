0e481af080f2f09f3d738660a72cc7ecb8725f18
Permafrozen <m8tt8o@gmail.com>
2024-09-10 20:31:00 +0200

    fixxed waybar again

--- external/config/desktop-environment/status-bar/waybar.nix
+++ external/config/desktop-environment/status-bar/waybar.nix
@@ -33,7 +33,6 @@ in {
       background: rgba(${config.lib.stylix.colors.base00-rgb-r}, ${config.lib.stylix.colors.base00-rgb-g}, ${config.lib.stylix.colors.base00-rgb-b}, ${vars.opacity});
       color: #${config.lib.stylix.colors.base05};
       border-bottom: 2px solid #${config.lib.stylix.colors.base03};
-      margin: 20px;
     }
 
     tooltip {
