ec2ccf7b682198fb848fe2394b5fab81d99cabeb
Permafrozen <m8tt8o@gmail.com>
2024-09-11 09:21:33 +0200

    improved json config of fastfetch

--- external/config/application/fastfetch/fastfetch.nix
+++ external/config/application/fastfetch/fastfetch.nix
@@ -10,16 +10,102 @@ in
 
   home-manager.sharedModules = [{
     home.file.".config/fastfetch/config.jsonc".text = ''
-      {
-          "$schema": "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json",
-          "modules": [
-              "title",
-              "separator",
-              "module1",
+    {
+    "$schema": "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json",
+    "logo": {
+    	"source": "~/.modular-nixos-configuration/external/theme/ascii-icon/rose.txt",
+    	"type": "kitty",
+    	"height": 18,
+    	"padding": {
+    		"top": 2
+    	}
+    },
+    "display": {
+        "separator": " "
+    },
+    "modules": [
+        "break",
+        "break",
+        "break",
+        {
+                  "type": "custom",
+                  "format": "\u001b[90m  \u001b[31m  \u001b[32m  \u001b[33m  \u001b[34m  \u001b[35m  \u001b[36m  \u001b[37m"
+              },
+        "break",
+        {
+                  "type": "title",
+                  "keyWidth": 10
+              },
+              "break",
               {
-                  "type": "module2",
-                  "module2-option": "value"
-              }
+                  "type": "os",
+                  "key": " ",
+                  "keyColor": "34",  // = color4
+              },
+              {
+                  "type": "kernel",
+                  "key": " ",
+                  "keyColor": "34",
+              },
+              {
+                  "type": "packages",
+                  "format": "{} (pacman)",
+                  "key": " ",
+                  "keyColor": "34",  
+              },
+              {
+                  "type": "shell",
+                  "key": " ",
+                  "keyColor": "34", 
+              },
+              {
+                  "type": "terminal",
+                  "key": " ",
+                  "keyColor": "34", 
+              },
+              {
+                  "type": "wm",
+                  "key": " ",
+                  "keyColor": "34", 
+              },
+              {
+                  "type": "cursor",
+                  "key": " ",
+                  "keyColor": "34", 
+              },
+              {
+                  "type": "terminalfont",
+                  "key": " ",
+                  "keyColor": "34", 
+              },
+              {
+                  "type": "uptime",
+                  "key": " ",
+                  "keyColor": "34", 
+              },
+              {
+                  "type": "datetime",
+                  "format": "{1}-{3}-{11}",
+                  "key": " ",
+                  "keyColor": "34", 
+              },
+              {
+                  "type": "media",
+                  "key": "󰝚 ",
+                  "keyColor": "34", 
+              },
+              {
+                  "type": "player",
+                  "key": " ",
+                  "keyColor": "34", 
+              },
+              "break",
+              {
+                  "type": "custom",
+                  "format": "\u001b[90m  \u001b[31m  \u001b[32m  \u001b[33m  \u001b[34m  \u001b[35m  \u001b[36m  \u001b[37m"
+              },
+              "break",
+              "break",
           ]
       }
     '';
