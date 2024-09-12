31b88e7d6f73ec07d815629c888d0c4d338815ff
Permafrozen <m8tt8o@gmail.com>
2024-09-12 10:33:46 +0200

    added config for rofi-wayland

--- external/config/desktop-environment/app-launcher/rofi-wayland.nix
+++ external/config/desktop-environment/app-launcher/rofi-wayland.nix
@@ -6,6 +6,20 @@
   ];
 
   home-manager.sharedModules = [{
-    
+    home.file.".config/rofi/config.rasi".text = ''
+      configuration {               
+      /*  modes: "window,run,ssh,drun";*/
+      /*  font: "mono 12";*/
+      /*  location: 0;*/
+      /*  yoffset: 0;*/
+      /*  xoffset: 0;*/
+      /*  fixed-num-lines: true;*/
+      ... cut ...
+      /*  ml-row-down: "ScrollDown";*/                                                                                        
+      /*  me-select-entry: "MousePrimary";*/                                                                                  
+      /*  me-accept-entry: "MouseDPrimary";*/                                                                                 
+      /*  me-accept-custom: "Control+MouseDPrimary";*/ 
+      }
+    '';
   }];
 }
