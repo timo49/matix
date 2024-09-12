6e02c5278a89621ec491647f480f27a9f6024592
Permafrozen <m8tt8o@gmail.com>
2024-09-12 09:47:52 +0200

    added notion to host

--- core/host/laptop.nix
+++ core/host/laptop.nix
@@ -26,6 +26,7 @@
     ../../external/config/application/browser/qutebrowser.nix
     ../../external/config/application/virtualisation/virt-manager.nix
     ../../external/config/application/notes/obsidian.nix
+    ../../external/config/application/notes/notion.nix
     ../../external/config/application/discord/vesktop.nix
     ../../external/config/application/bottom/bottom.nix
 
