a7af659f2a8bfd7aa999028882e7d1e1a27eb3e5
Permafrozen <m8tt8o@gmail.com>
2024-09-16 18:47:23 +0200

    added config for myself into users

---
+++ users/matteo/variables.nix
@@ -0,0 +1,28 @@
+{
+  # System Information
+  username = "matteo";
+  keyMap = "at";
+
+  # Theme
+  colorScheme = "gruvbox-dark-soft";
+  polarity = "dark";
+  opacity = "0.7";
+  rounding = "15";
+  borderWidth = "0";
+
+  # Hyprland
+  resolution = "1920x1080@60";
+  terminal = "kitty";
+  browser = "qutebrowser";
+  fileManager = "dolphin";
+  menu = "rofi";
+
+  # Hyprpaper (Wallpaper)
+  wallpaper= "forest-green.png"; # external/theme/wallpaper
+
+  # Git Configuration
+  gitUser = "Permafrozen";
+  gitMail = "m8tt8o@gmail.com";
+  defaultBranch = "main";
+
+}
