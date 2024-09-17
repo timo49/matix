e01eac40251509cc3f4a14f50722584d4903ae6a
GluttonMain <frozenkill.hd@gmail.com>
2024-09-17 11:36:39 +0200

    editing git-pull.sh so that it only requires 3 arguments

--- bash/git-pull.sh
+++ bash/git-pull.sh
@@ -1,16 +1,16 @@
 #!/run/current-system/sw/bin/bash
 
 if [ "$1" = "--help" ]; then
-    echo "git-push.sh -> <user> -> <msg> -> <repo> -> <branch>"
+    echo "git-push.sh -> <user> -> <repo> -> <branch>"
 fi
 if [ "$1" = "timo" ] || [ "$1" = "matteo" ]; then 
     rm -f ~/.modular-nixos-configuration/users/user.nix 
     
     git add -A
     
-    git commit -m "$2"
+    git commit -m "getting current config-version"
     
-    git pull --rebase $3 $4
+    git pull --rebase $2 $3
     
     ~/.modular-nixos-configuration/bash/userQuery.sh $1
 
