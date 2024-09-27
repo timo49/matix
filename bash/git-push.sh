3dace06188c8821e519d1aa4559be84962b2245f
GluttonMain <frozenkill.hd@gmail.com>
2024-09-27 09:16:09 +0200

    edited git-push.sh, so that you are required to add and commit berfore using it

--- bash/git-push.sh
+++ bash/git-push.sh
@@ -7,10 +7,10 @@ if [ "$1" = "timo" ] || [ "$1" = "matteo" ]; then
     rm -f ~/.modular-nixos-configuration/users/user.nix 
     
     git add -A
+
+    git commit -m "pushing"
     
-    git commit -m "$2"
-    
-    git push -u $3 $4
+    git push -u $2 $3
     
     ~/.modular-nixos-configuration/bash/userQuery.sh $1
 
